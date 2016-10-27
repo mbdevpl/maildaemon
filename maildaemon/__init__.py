0ed103c96a2179952d5f13dd9df44762b9961012
Mateusz Bysiek <mb@mbdev.pl>
2016-10-28 00:12:22 +0900

    added default logging settings
    Signed-off-by: Mateusz Bysiek <mb@mbdev.pl>

--- maildaemon/__init__.py
+++ maildaemon/__init__.py
@@ -1,3 +1,21 @@
 """
 This is "__init__.py" file of maildaemon module.
 """
+
+import logging
+import logging.config
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'brief': {'style': '{', 'format': '{name} [{levelname}] {message}'},
+        'precise': {'style': '{', 'format': '{asctime} {name} [{levelname}] {message}'}
+        },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler', 'formatter': 'brief', 'level': logging.NOTSET,
+            'stream': 'ext://sys.stdout'}
+        },
+    'root': {'level': logging.DEBUG, 'handlers': ['console']}
+    })
