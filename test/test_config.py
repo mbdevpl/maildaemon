6138f5d899529077b864aeb488d0c9e5b9961b37
Mateusz Bysiek <mb@mbdev.pl>
2016-10-28 00:17:04 +0900

    more tests for config
    Signed-off-by: Mateusz Bysiek <mb@mbdev.pl>

--- test/test_config.py
+++ test/test_config.py
@@ -1,9 +1,17 @@
 
+import ast
 import unittest
 
 from maildaemon.config import load_config
 
 class TestConfig(unittest.TestCase):
+
+    def test_methodology(self):
+
+        #self.assertTrue(bool('True')) # correct by accident
+        #self.assertFalse(bool('False')) # wrong
+        self.assertTrue(ast.literal_eval('True'))
+        self.assertFalse(ast.literal_eval('False'))
 
     def test_load_config_basic(self):
 
@@ -19,3 +27,16 @@ class TestConfig(unittest.TestCase):
 
         self.assertIn('gmail-imap', cfg['connections'], msg=cfg)
         self.assertIn('gmail-smtp', cfg['connections'], msg=cfg)
+
+    def test_load_config_completeness(self):
+
+        cfg = load_config()
+
+        self.assertIn('gmail-imap', cfg['connections'], msg=cfg)
+
+        section = cfg['connections']['gmail-imap']
+        self.assertIn('domain', section, msg=cfg)
+        self.assertIn('ssl', section, msg=cfg)
+        self.assertIn('port', section, msg=cfg)
+        self.assertIn('login', section, msg=cfg)
+        self.assertIn('password', section, msg=cfg)
