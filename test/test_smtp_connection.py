5cfc3a6c336230ee502c0380a2f5f10090ef9367
Mateusz Bysiek <mb@mbdev.pl>
2016-10-30 19:35:49 +0900

    tests for SMTP connection
    Signed-off-by: Mateusz Bysiek <mb@mbdev.pl>

---
+++ test/test_smtp_connection.py
@@ -0,0 +1,43 @@
+
+import email
+import unittest
+
+from maildaemon.config import load_config
+from maildaemon.imap_connection import IMAPConnection
+from maildaemon.smtp_connection import SMTPConnection
+
+class Test(unittest.TestCase):
+
+    config = load_config()
+
+    def test_connect(self):
+
+        s = SMTPConnection.from_dict(self.config['connections']['gmail-smtp'])
+        s.connect()
+        s.disconnect()
+
+    def test_send_message(self):
+
+        c = IMAPConnection.from_dict(self.config['connections']['1and1-imap'])
+        c.connect()
+        _, body = c.retrieve_message_parts(1, ['BODY.PEEK[]'])
+        c.disconnect()
+
+        message = email.message_from_bytes(body)
+
+        #print(message.as_string())
+        #print(message.keys())
+
+        s = SMTPConnection.from_dict(self.config['connections']['gmail-smtp'])
+        s.connect()
+
+        del message['Subject']
+        message['Subject'] = 'test'
+        del message['From']
+        message['From'] = s.login
+        del message['To']
+        message['To'] = s.login
+        #message['Envelope-To'] = s.login
+        s.send_message(message)
+
+        s.disconnect()
