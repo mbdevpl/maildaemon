31289db62e9407e5e3cc6b995677aac899015535
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-06-04 16:12:21 +0900

    refactored configuration loader to use JSON

--- maildaemon/config.py
+++ maildaemon/config.py
@@ -1,76 +1,67 @@
+"""JSON configuration reader."""
 
-import ast
-import configparser
+import json
+import pathlib
 
-DEFAULT_CONFIG_PATH = '.maildaemon.config'
+# from ingit.json_config import file_to_json
+
+DEFAULT_CONFIG_PATH = pathlib.Path('maildaemon_config.json')
 
 
-def load_config(path: str = DEFAULT_CONFIG_PATH):
+def str_to_json(text: str) -> dict:
+    """Convert JSON string into an object."""
+    try:
+        return json.loads(text)
+    except json.decoder.JSONDecodeError as err:
+        lines = text.splitlines(keepends=True)
+        raise ValueError('\n{}{}\n{}'.format(
+            ''.join(lines[max(0, err.lineno - 10):err.lineno]), '-' * err.colno,
+            ''.join(lines[err.lineno:min(err.lineno + 10, len(lines))]))) from err
 
-    config = configparser.ConfigParser()
 
-    config.read(path)
+def file_to_json(path: pathlib.Path) -> dict:
+    """Create JSON object from a file."""
+    with open(str(path), 'r', encoding='utf-8') as json_file:
+        text = json_file.read()
+    try:
+        data = str_to_json(text)
+    except ValueError as err:
+        raise ValueError('in file "{}"'.format(path)) from err
+    return data
 
-    connections = {}
-    filters = {}
 
-    for section, contents in config.items():
+def load_config(path: pathlib.Path = DEFAULT_CONFIG_PATH):
+    """Load maildaemon configuration from file."""
+    config = file_to_json(path)
+    try:
+        validate_config(config)
+    except AssertionError as err:
+        raise ValueError('{} is an invalid maildaemon config'.format(path)) from err
+    return config
 
-        if section.startswith('connection:'):
-            connection_name = section.replace('connection:', '', 1).strip()
-            connection = {}
-            try:
-                connection['protocol'] = contents['protocol']
-            except KeyError as err:
-                raise RuntimeError(
-                    'no protocol provided in configuration for connection "{}"'
-                    .format(connection_name)) from err
-            try:
-                connection['domain'] = contents['domain']
-            except KeyError as err:
-                raise RuntimeError(
-                    'no domain provided in configuration for connection "{}"'
-                    .format(connection_name)) from err
-            try:
-                connection['ssl'] = ast.literal_eval(contents['ssl'])
-            except KeyError:
-                pass
-            try:
-                connection['port'] = int(contents['port'])
-            except KeyError:
-                pass
-            try:
-                connection['login'] = contents['login']
-            except KeyError:
-                pass
-            try:
-                connection['password'] = contents['password']
-            except KeyError:
-                pass
-            connections[connection_name] = connection
 
-        if section.startswith('filter:'):
-            filter_name = section.replace('filter:', '', 1).strip()
-            filter_ = {}
-            try:
-                filter_['connections'] = [c.strip() for c in contents['connections'].split(',')]
-            except KeyError:
-                pass
-            try:
-                filter_['condition'] = [
-                    [c.strip() for c in d.strip().split(' and ')]
-                    for d in contents['condition'].split(' or ')]
-            except KeyError:
-                pass
-            try:
-                filter_['actions'] = [a.strip() for a in contents['actions'].split(',')]
-            except KeyError as err:
-                raise RuntimeError(
-                    'no action provided in configuration for filter "{}"'
-                    .format(filter_name)) from err
-            filters[filter_name] = filter_
-
-    return {
-        'connections': connections,
-        'filters': filters
-        }
+def validate_config(config: dict):
+    """Validate the maildaemon configuration."""
+    for name, connection in config.get('connections', {}).items():
+        assert isinstance(name, str), type(name)
+        assert name, name
+        assert 'protocol' in connection, connection
+        assert isinstance(connection['protocol'], str), type(connection['protocol'])
+        assert connection['protocol'], connection
+        assert 'domain' in connection, connection
+        assert isinstance(connection['domain'], str), type(connection['domain'])
+        assert connection['domain'], connection
+        assert isinstance(connection.get('ssl', False), int), type(connection['port'])
+        assert isinstance(connection.get('port', 1), int), type(connection['port'])
+        assert connection.get('port', 1) > 0, connection['port']
+        assert isinstance(connection.get('login', 'test'), str), type(connection['login'])
+        assert connection.get('login', 'test'), connection['login']
+        assert isinstance(connection.get('password', 'test'), str), type(connection['password'])
+        assert connection.get('password', 'test'), connection['password']
+    for name, filter_ in config.get('filters', {}).items():
+        for connection_name in filter_.get('connections', []):
+            assert connection_name in config['connections']
+        assert 'condition' in filter_, filter_
+        assert isinstance(filter_['condition'], str), type(filter_['condition'])
+        for action in filter_.get('actions', []):
+            assert isinstance(action, str), type(action)
--- test/test_config.py
+++ test/test_config.py
@@ -1,5 +1,5 @@
+"""Test configuration loader."""
 
-import ast
 import os
 import unittest
 
@@ -8,34 +8,21 @@ from maildaemon.config import load_config
 
 class Tests(unittest.TestCase):
 
-    def test_methodology(self):
-
-        # self.assertTrue(bool('True')) # correct by accident
-        # self.assertFalse(bool('False')) # wrong
-        self.assertTrue(ast.literal_eval('True'))
-        self.assertFalse(ast.literal_eval('False'))
-
-    def test_load_config_basic(self):
-
+    def test_load_basic(self):
         cfg = load_config()
         self.assertIn('connections', cfg, msg=cfg)
         self.assertGreater(len(cfg['connections']), 0, msg=cfg)
 
-    def test_load_config_concrete_connections(self):
-
+    def test_load_concrete_connections(self):
         cfg = load_config()
         self.assertIn('connections', cfg, msg=cfg)
         self.assertGreater(len(cfg['connections']), 0, msg=cfg)
-
         self.assertIn('test-imap', cfg['connections'], msg=cfg)
-        # self.assertIn('gmail-smtp', cfg['connections'], msg=cfg)
+        self.assertIn('test-pop', cfg['connections'], msg=cfg)
 
-    def test_load_config_completeness(self):
-
+    def test_completeness(self):
         cfg = load_config()
-
         self.assertIn('test-imap', cfg['connections'], msg=cfg)
-
         section = cfg['connections']['test-imap']
         self.assertIn('domain', section, msg=cfg)
         self.assertIn('ssl', section, msg=cfg)
@@ -43,41 +30,27 @@ class Tests(unittest.TestCase):
         self.assertIn('login', section, msg=cfg)
         self.assertIn('password', section, msg=cfg)
 
-    def test_config_missing_protocol(self):
-
+    def test_missing_protocol(self):
         config_filename = '.maildaemon.config.tmp'
-
-        with open(config_filename, 'w') as f:
-            print('''[connection: missing-protocol]\n  domain = example.com''', file=f)
-
-        with self.assertRaises(RuntimeError):
-            cfg = load_config(config_filename)
-            self.assertIsNone(cfg, msg=cfg)
-
+        with open(config_filename, 'w') as cfg_file:
+            print(r'''{"connections": {"missing-protocol": {"domain": "example.com"}}}''',
+                  file=cfg_file)
+        with self.assertRaises(ValueError):
+            load_config(config_filename)
         os.remove(config_filename)
 
-    def test_config_missing_domain(self):
-
+    def test_missing_domain(self):
         config_filename = '.maildaemon.config.tmp'
-
-        with open(config_filename, 'w') as f:
-            print('''[connection: missing-domain]\n  protocol = IMAP''', file=f)
-
-        with self.assertRaises(RuntimeError):
-            cfg = load_config(config_filename)
-            self.assertIsNone(cfg, msg=cfg)
-
+        with open(config_filename, 'w') as cfg_file:
+            print(r'''{"connections": {"missing-protocol": {"protocol": "IMAP"}}}''', file=cfg_file)
+        with self.assertRaises(ValueError):
+            load_config(config_filename)
         os.remove(config_filename)
 
-    def test_config_missing_filter_action(self):
-
+    def test_missing_filter_action(self):
         config_filename = '.maildaemon.config.tmp'
-
-        with open(config_filename, 'w') as f:
-            print('''[filter: empty]''', file=f)
-
-        with self.assertRaises(RuntimeError):
-            cfg = load_config(config_filename)
-            self.assertIsNone(cfg, msg=cfg)
-
+        with open(config_filename, 'w') as cfg_file:
+            print(r'''{"filters": {"empty": {}}}''', file=cfg_file)
+        with self.assertRaises(ValueError):
+            load_config(config_filename)
         os.remove(config_filename)
