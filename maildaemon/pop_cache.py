3227f51f11c3cfaa2433dfc5b1da24d579a30032
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2019-10-25 00:32:31 +0900

    reworking PopCache to use EmailCache

--- maildaemon/pop_cache.py
+++ maildaemon/pop_cache.py
@@ -5,23 +5,28 @@ import logging
 import typing as t
 
 from .message import Message
-from .daemon import Daemon
+from .folder import Folder
+from .email_cache import EmailCache
 from .pop_connection import POPConnection
 
 _LOG = logging.getLogger(__name__)
 
 
-class POPCache(Daemon, POPConnection):
-
+class POPCache(EmailCache, POPConnection):
     """Daemon working with POP connections."""
 
     def __init__(self, domain: str, port: t.Optional[int] = None, ssl: bool = True):
-        Daemon.__init__(self)
+        EmailCache.__init__(self)
         POPConnection.__init__(self, domain, port, ssl)
+        # self.folders = ['INBOX']  # type: t.List[str]
+        # self.message_ids = {'INBOX': []}  # type: t.Mapping[str, t.List[int]]
+        # self.messages = {}  # type: t.Mapping[t.Tuple[str, int], Message]
 
-        self.folders = ['INBOX']  # type: t.List[str]
-        self.message_ids = {'INBOX': []}  # type: t.Mapping[str, t.List[int]]
-        self.messages = {}  # type: t.Mapping[t.Tuple[str, int], Message]
+    def update_folders(self):
+        if not self.folders:
+            self.folders['INBOX'] = Folder(self, 'INBOX')
+        assert len(self.folders) == 1, len(self.folders)
+        assert 'INBOX' in self.folders, self.folders
 
     def retrieve_messages(self, message_ids: t.List[int]) -> t.List[Message]:
         messages = []
@@ -44,30 +49,33 @@ class POPCache(Daemon, POPConnection):
 
         return Message(email_message, self, None, message_id)
 
-    def _update_messages(self):
+    def update_messages_in(self, folder: Folder):
+        assert folder.name == 'INBOX', folder
+
         message_ids = self.retrieve_message_ids()
-        new_message_ids = []
+        # new_message_ids = []
 
-        for message_id in self.message_ids['INBOX']:
-            if message_id not in message_ids:
-                _LOG.warning('%s: message #%i was deleted', self, message_id)
-                self.message_ids['INBOX'].remove(message_id)
-                del self.messages['INBOX', message_id]
+        # for message_id in self.message_ids['INBOX']:
+        #     if message_id not in message_ids:
+        #         _LOG.warning('%s: message #%i was deleted', self, message_id)
+        #         self.message_ids['INBOX'].remove(message_id)
+        #         del self.messages['INBOX', message_id]
 
-        for message_id in message_ids:
-            if message_id not in self.message_ids['INBOX']:
-                _LOG.info('%s: new message #%i found', self, message_id)
-                self.message_ids['INBOX'].append(message_id)
-                new_message_ids.append(message_id)
+        # for message_id in message_ids:
+        #     if message_id not in self.message_ids['INBOX']:
+        #         _LOG.info('%s: new message #%i found', self, message_id)
+        #         self.message_ids['INBOX'].append(message_id)
+        #         new_message_ids.append(message_id)
 
-        new_messages = self.retrieve_messages(new_message_ids)
+        # new_messages = self.retrieve_messages(new_message_ids)
 
-        for new_message_id, new_message in zip(new_message_ids, new_messages):
-            if ('INBOX', new_message_id) in self.messages:
-                _LOG.error('%s: message #%i is not really new\ncurrent:\n%s\nnew:\n%s',
-                           self, new_message_id, repr(self.messages['INBOX', new_message_id]),
-                           repr(new_message))
-            self.messages['INBOX', new_message_id] = new_message
+        messages = self.retrieve_messages(message_ids)
+        for message in messages:
+            folder.add_message(message)
 
-    def update(self):
-        self._update_messages()
+        # for new_message_id, new_message in zip(new_message_ids, new_messages):
+        #     if ('INBOX', new_message_id) in self.messages:
+        #         _LOG.error('%s: message #%i is not really new\ncurrent:\n%s\nnew:\n%s',
+        #                    self, new_message_id, repr(self.messages['INBOX', new_message_id]),
+        #                    repr(new_message))
+        #     self.messages['INBOX', new_message_id] = new_message
