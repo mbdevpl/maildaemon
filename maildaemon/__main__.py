b85af357e12d5baa55d4cda9aceec867e7653ceb
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2023-03-18 19:34:11 +0900

    chore: resolve some linting issues

--- maildaemon/__main__.py
+++ maildaemon/__main__.py
@@ -1,6 +1,4 @@
-"""
-This is "__main__.py" file of maildaemon module.
-"""
+"""Entry point of maildaemon package."""
 
 import argparse
 import logging
@@ -26,66 +24,64 @@ def parse_args(args=None):
     """Parse command-line arguments."""
     parser = argparse.ArgumentParser(
         prog='maildaemon',
-        description='''maildaemon: multi-server mail filtering daemon supporting IMAP, POP and SMTP.
+        description=f'''Multi-server mail filtering daemon supporting IMAP, POP and SMTP.
 
-Configuration is stored in "{}".
+Configuration is stored in "{DEFAULT_CONFIG_PATH}".
 
-'''.format(DEFAULT_CONFIG_PATH), epilog='''examples:
+''', epilog='''examples:
   maildaemon -h
   maildaemon -d
 
-Copyright 2016-2020  Mateusz Bysiek  https://mbdevpl.github.io/
+Copyright 2016-2023  Mateusz Bysiek  https://mbdevpl.github.io/
 ''',
         formatter_class=Formatter, allow_abbrev=True)
-    """
-    ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[],
-        formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None,
-        argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True)
-
-    prog - The name of the program (default: sys.argv[0])
-    usage - The string describing the program usage (default: generated from arguments added to
-        parser)
-    description - Text to display before the argument help (default: none)
-    epilog - Text to display after the argument help (default: none)
-    parents - A list of ArgumentParser objects whose arguments should also be included
-    formatter_class - A class for customizing the help output
-    prefix_chars - The set of characters that prefix optional arguments (default: ‘-‘)
-    fromfile_prefix_chars - The set of characters that prefix files from which additional arguments
-        should be read (default: None)
-    argument_default - The global default value for arguments (default: None)
-    conflict_handler - The strategy for resolving conflicting optionals (usually unnecessary)
-    add_help - Add a -h/–help option to the parser (default: True)
-    allow_abbrev - Allows long options to be abbreviated if the abbreviation is unambiguous.
-        (default: True)
-
-    formatter classes:
-        class argparse.RawDescriptionHelpFormatter
-        class argparse.RawTextHelpFormatter
-        class argparse.ArgumentDefaultsHelpFormatter
-        class argparse.MetavarTypeHelpFormatter
-
-    add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices]
-        [, required][, help][, metavar][, dest])
-
-    name or flags - Either a name or a list of option strings, e.g. foo or -f, --foo.
-    action - The basic type of action to be taken when this argument is encountered at the command
-        line.
-    nargs - The number of command-line arguments that should be consumed.
-    const - A constant value required by some action and nargs selections.
-    default - The value produced if the argument is absent from the command line.
-    type - The type to which the command-line argument should be converted.
-    choices - A container of the allowable values for the argument.
-    required - Whether or not the command-line option may be omitted (optionals only).
-    help - A brief description of what the argument does.
-    metavar - A name for the argument in usage messages.
-    dest - The name of the attribute to be added to the object returned by parse_args().
-
-    action:
-        'store', 'store_const', 'store_true', 'store_false', 'append', 'append_const', 'count',
-        'help', 'version'
-    nargs:
-        N, '?', '*', '+'
-    """
+    # ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[],
+    #     formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None,
+    #     argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True)
+    #
+    # prog - The name of the program (default: sys.argv[0])
+    # usage - The string describing the program usage (default: generated from arguments added to
+    #     parser)
+    # description - Text to display before the argument help (default: none)
+    # epilog - Text to display after the argument help (default: none)
+    # parents - A list of ArgumentParser objects whose arguments should also be included
+    # formatter_class - A class for customizing the help output
+    # prefix_chars - The set of characters that prefix optional arguments (default: ‘-‘)
+    # fromfile_prefix_chars - The set of characters that prefix files from which additional
+    #     arguments should be read (default: None)
+    # argument_default - The global default value for arguments (default: None)
+    # conflict_handler - The strategy for resolving conflicting optionals (usually unnecessary)
+    # add_help - Add a -h/–help option to the parser (default: True)
+    # allow_abbrev - Allows long options to be abbreviated if the abbreviation is unambiguous.
+    #     (default: True)
+    #
+    # formatter classes:
+    #     class argparse.RawDescriptionHelpFormatter
+    #     class argparse.RawTextHelpFormatter
+    #     class argparse.ArgumentDefaultsHelpFormatter
+    #     class argparse.MetavarTypeHelpFormatter
+    #
+    # add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices]
+    #     [, required][, help][, metavar][, dest])
+    #
+    # name or flags - Either a name or a list of option strings, e.g. foo or -f, --foo.
+    # action - The basic type of action to be taken when this argument is encountered at the command
+    #     line.
+    # nargs - The number of command-line arguments that should be consumed.
+    # const - A constant value required by some action and nargs selections.
+    # default - The value produced if the argument is absent from the command line.
+    # type - The type to which the command-line argument should be converted.
+    # choices - A container of the allowable values for the argument.
+    # required - Whether or not the command-line option may be omitted (optionals only).
+    # help - A brief description of what the argument does.
+    # metavar - A name for the argument in usage messages.
+    # dest - The name of the attribute to be added to the object returned by parse_args().
+    #
+    # action:
+    #     'store', 'store_const', 'store_true', 'store_false', 'append', 'append_const', 'count',
+    #     'help', 'version'
+    # nargs:
+    #     N, '?', '*', '+'
 
     parser.version = version
 
